036df9c71dcbb31ac031de491ef976ee3c8559f7
Matan <matanyakobovich@yahoo.com>
2023-04-22 17:50:43 +0200

    Lifecycle hooks + passing data between components.

---
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,45 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { RoomList } from '../rooms';
+
+@Component({
+  selector: 'app-rooms-list',
+  templateUrl: './rooms-list.component.html',
+  styleUrls: ['./rooms-list.component.css'],
+  // changeDetection onPush Improves performance by only updating the component that has changed i.e. replaced not simply edited
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+  
+// Can only use ngOnChanges if you have an input property
+export class RoomsListComponent implements OnInit, OnChanges {
+  @Input() rooms: RoomList[] = [];
+
+  @Input() title: string = "";
+
+  @Output() selectedRoom = new EventEmitter<RoomList>();
+
+  constructor() {}
+
+  // Will be called when the input property changes
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (changes['title']) {
+      this.title = changes['title'].currentValue.toUpperCase();
+    }
+  }
+
+  ngOnInit(): void {}
+
+  selectRoom(room: RoomList) {
+    // Following line will give the data back to the parent component
+    this.selectedRoom.emit(room);
+  }
+}
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -19,47 +19,82 @@ export class RoomsComponent implements OnInit {
     bookedRooms: 5,
   };
 
-  roomList: RoomList[] = [
-    {
-      roomId: 1,
-      roomType: 'Single',
+  title = 'Room List';
+
+  roomList: RoomList[] = [];
+
+  selectedRoom!: RoomList;
+
+  // Constructor is not for logic only for dependency injection
+  constructor() {}
+
+  // ngOnInit is a lifecycle hook that gets called after the constructor
+  // Use ngOnInit after the component is initialized
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomId: 1,
+        roomType: 'Single',
+        amenities: 'TV, Aircon, Wifi',
+        price: 100,
+        photos:
+          'https://pix10.agoda.net/hotelImages/79114/-1/e98e5ebcfc0bbb7175ad4d3c9c4aa71e.jpg?ca=19&ce=1&s=1024x768',
+        checkInTime: new Date('2021-01-01'),
+        checkOutTime: new Date('2021-01-02'),
+        rating: 4.5,
+      },
+      {
+        roomId: 2,
+        roomType: 'Double',
+        amenities: 'TV, Aircon, Wifi',
+        price: 200,
+        photos:
+          'https://hiltonsandton.hotels-johannesburg.com/data/Photos/OriginalPhoto/12783/1278399/1278399076/johannesburg-hilton-sandton-hotel-photo-2.JPEG',
+        checkInTime: new Date('2021-01-01'),
+        checkOutTime: new Date('2021-01-02'),
+        rating: 3.4123121,
+      },
+      {
+        roomId: 3,
+        roomType: 'Family',
+        amenities: 'TV, Aircon, Wifi',
+        price: 300,
+        photos:
+          'https://hilton-sandton-hotel-johannesburg.booked.net/data/Photos/OriginalPhoto/12784/1278401/1278401350/Hilton-Sandton-Hotel-Johannesburg-Exterior.JPEG',
+        checkInTime: new Date('2021-01-01'),
+        checkOutTime: new Date('2021-01-02'),
+        rating: 2.6231231,
+      },
+    ];
+  }
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+    this.title = 'Rooms List';
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room;
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomId: 4,
+      roomType: 'Deluxe',
       amenities: 'TV, Aircon, Wifi',
-      price: 100,
+      price: 400,
       photos:
         'https://pix10.agoda.net/hotelImages/79114/-1/e98e5ebcfc0bbb7175ad4d3c9c4aa71e.jpg?ca=19&ce=1&s=1024x768',
       checkInTime: new Date('2021-01-01'),
       checkOutTime: new Date('2021-01-02'),
       rating: 4.5,
-    },
-    {
-      roomId: 2,
-      roomType: 'Double',
-      amenities: 'TV, Aircon, Wifi',
-      price: 200,
-      photos:
-        'https://hiltonsandton.hotels-johannesburg.com/data/Photos/OriginalPhoto/12783/1278399/1278399076/johannesburg-hilton-sandton-hotel-photo-2.JPEG',
-      checkInTime: new Date('2021-01-01'),
-      checkOutTime: new Date('2021-01-02'),
-      rating: 3.4123121,
-    },
-    {
-      roomId: 3,
-      roomType: 'Family',
-      amenities: 'TV, Aircon, Wifi',
-      price: 300,
-      photos:
-        'https://hilton-sandton-hotel-johannesburg.booked.net/data/Photos/OriginalPhoto/12784/1278401/1278401350/Hilton-Sandton-Hotel-Johannesburg-Exterior.JPEG',
-      checkInTime: new Date('2021-01-01'),
-      checkOutTime: new Date('2021-01-02'),
-      rating: 2.6231231,
-    },
-  ];
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  toggle() {
-    this.hideRooms = !this.hideRooms;
+    };
+    // In contrast to the push method, the spread operator does not mutate
+    // the original array.Instead, it creates a new array instance, which
+    // is assigned back to the roomList property.This means that the roomList
+    // property is effectively replaced with a new array that contains the
+    // original elements plus the new room object.
+    // this.roomList.push(room);
+    this.roomList = [...this.roomList, room];
   }
 }
